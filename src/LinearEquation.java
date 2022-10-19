96acb46ff50b12d6e49b1151ced7596f43ed9681
Felix Chan <felixc63@nycstudents.net>
2022-10-19 12:32:22 -0400

    almost

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -10,4 +10,78 @@ public class LinearEquation {
         this.y1 = y1;
         this.y2 = y2;
     }
+
+    public double round(double x){
+        return Math.round(x*100)/100.0;
+    }
+    public double slope (){
+        return round((double)(y2-y1)/(x2-x1));
+    }
+
+    public double yIntercept(){
+        return round((double)y1-(slope()*x1));
+    }
+
+    public double distance(){
+        return round(Math.sqrt(Math.pow((double)x2-x1,2) + Math.pow((double)y2-y1,2)));
+    }
+
+    public String equation(){
+        String m = "";
+        String b;
+        int xDiff = x2-x1;
+        int yDiff = y2-y1;
+        if (slope() == 1) {
+            m = "";
+        } else if (slope() == -1) {
+            m = "-";
+        } else{
+            if (slope() % 1 == 0) {
+                m = Double.toString(slope());
+            } else {
+                if ((xDiff > 0 && yDiff > 0) || (xDiff < 0 && yDiff < 0)) {
+                    m = Math.abs(yDiff) + "/" + Math.abs(xDiff);
+                } else if ((xDiff > 0 && yDiff < 0) || (xDiff < 0 && yDiff > 0)) {
+                    m = "-" + Math.abs(yDiff) + "/" + Math.abs(xDiff);
+                }
+            }
+        }
+        if (yIntercept() == 0){
+            b = "";
+        }
+        else{
+            if (yIntercept() < 0){
+                if (slope() == 0){
+                    b = "-" + Double.toString(yIntercept());
+                }else {
+                    b = "- " + Double.toString(yIntercept());
+                }
+            }
+            else {
+                if (slope() == 0){
+                    b = Double.toString(yIntercept());
+                } else {
+                    b = "+ " + Double.toString(yIntercept());
+                }
+            }
+        }
+        if (slope() == 0){
+            return "y = " + b;
+        }else{
+            return "y = " + m + "x " + b;
+        }
+    }
+
+    public String coordinateForX(double xValue){
+        return "(" + xValue + ", " + ((slope()*xValue)+yIntercept()) + ")";
+    }
+
+    public void lineInfo(){
+        System.out.println("The two points are: (" + x1 + ", " + y1 + ") and (" + x2 + ", " + y2 + ")" );
+        System.out.println("The equation of the line between these points is: " + equation());
+        System.out.println("The slope of this line is: " + slope());
+        System.out.println("The y-intercept of the line is: " + yIntercept());
+        System.out.println("The distance between the two points is: " + distance());
+    }
 }
+
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -2,10 +2,6 @@ import java.util.Scanner;
 
 public class LinearEquationRunner {
 
-private static int x1;
-private static int x2;
-private static int y1;
-private static int y2;
 
 
     public static void main(String[] args){
@@ -23,32 +19,16 @@ private static int y2;
 
         int x2 = Integer.parseInt(coord2.substring(1,coord2.indexOf(",")));
         int y2 = Integer.parseInt(coord2.substring(coord2.indexOf(",")+2,coord2.length()-1));
-        /*String firstHalfCoord1 = coord1.substring(0,4);
-        if (firstHalfCoord1.contains("-")){
-            x1 = Integer.parseInt(firstHalfCoord1.substring(1,3));
-            y1 = Integer.parseInt(coord1.substring(5,coord1.length()-1));
-        }
-        else{
-            x1 = Integer.parseInt(firstHalfCoord1.substring(1,2));
-            y1 = Integer.parseInt(coord1.substring(4,coord1.length()-1));
-        }
 
-
-        String firstHalfCoord2 = coord2.substring(0,4);
-        if (firstHalfCoord2.contains("-")){
-            x2 = Integer.parseInt(firstHalfCoord2.substring(1,3));
-            y2 = Integer.parseInt(coord2.substring(5,coord2.length()-1));
+        if (x1 == x2){
+            System.out.println("You cannot have the same x values." + "\nPlease try again.");
+            System.exit(0);
         }
-        else{
-            x2 = Integer.parseInt(firstHalfCoord2.substring(1,2));
-            y2 = Integer.parseInt(coord2.substring(4,coord2.length()-1));
-        }*/
 
         LinearEquation linearEquation = new LinearEquation(x1,x2,y1,y2);
+        System.out.println();
+        linearEquation.lineInfo();
 
-        System.out.println(x1);
-        System.out.println(y1);
-        System.out.println(x2);
-        System.out.println(y2);
+
     }
 }
